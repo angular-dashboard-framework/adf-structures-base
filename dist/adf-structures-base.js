56fe1c9d0ec13f216534546d0e0eaaa5855eebe2
Sytone <github@sytone.com>
2015-10-01 11:54:48 -0700

    Addition of new layouts and updated dist.

--- dist/adf-structures-base.js
+++ dist/adf-structures-base.js
@@ -27,6 +27,63 @@ angular.module('adf.structures.base', ['adf'])
   .config(function(dashboardProvider){
 
     dashboardProvider
+      .structure('3-3-3', {
+        rows: [{
+          columns: [{
+            styleClass: 'col-md-3'
+          }, {
+            styleClass: 'col-md-3'
+          }, {
+            styleClass: 'col-md-3'
+          }]
+        }]
+      })   
+      .structure('2-2-2-2-2-2', {
+        rows: [{
+          columns: [{
+            styleClass: 'col-md-2'
+          }, {
+            styleClass: 'col-md-2'
+          }, {
+            styleClass: 'col-md-2'
+          }, {
+            styleClass: 'col-md-2'
+          }, {
+            styleClass: 'col-md-2'
+          }, {
+            styleClass: 'col-md-2'
+          }]
+        }]
+      })    
+      .structure('1-1-1-1-1-1-1-1-1-1-1-1', {
+        rows: [{
+          columns: [{
+            styleClass: 'col-md-1'
+          }, {
+            styleClass: 'col-md-1'
+          }, {
+            styleClass: 'col-md-1'
+          }, {
+            styleClass: 'col-md-1'
+          }, {
+            styleClass: 'col-md-1'
+          }, {
+            styleClass: 'col-md-1'
+          }, {
+            styleClass: 'col-md-1'            
+          }, {
+            styleClass: 'col-md-1'            
+          }, {
+            styleClass: 'col-md-1'            
+          }, {
+            styleClass: 'col-md-1'            
+          }, {
+            styleClass: 'col-md-1'            
+          }, {
+            styleClass: 'col-md-1'            
+          }]
+        }]
+      })         
       .structure('6-6', {
         rows: [{
           columns: [{
